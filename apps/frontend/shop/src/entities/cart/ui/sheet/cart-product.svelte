c8998414d15700ea14f82c334641ad5fb1b04b2d
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2025-05-08 00:08:24 +0300

    feat: quantity counter in cart

---
+++ apps/frontend/shop/src/entities/cart/ui/sheet/cart-product.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import type { Product } from '~/entities/product';
+  import { MOCK_PRODUCTS } from '~/entities/product/config/mock-products';
+  import { cartStore } from '../../lib/store/cart-store';
+  import NumberFlow from '@number-flow/svelte';
+
+  const { product }: { product: Product } = $props();
+
+  const getItemTotalPrice = (itemId: string) => {
+    const product = MOCK_PRODUCTS.find((product) => product.id === itemId);
+    const quantity = $cartStore.find((item) => item.id === itemId)?.quantity || 0;
+    return (product?.price || 0) * quantity;
+  };
+
+  const quantity = $derived($cartStore.find((item) => item.id === product.id)?.quantity || 0);
+
+  const inc = () => {
+    cartStore.updateQuantity(product.id, quantity + 1);
+  };
+
+  const dec = () => {
+    cartStore.updateQuantity(product.id, quantity - 1);
+  };
+</script>
+
+<div class="flex items-center gap-4">
+  <img src={product.imageUrl} alt={product.name} class="size-16 rounded-md object-cover shrink-0" />
+  <div class="flex flex-col gap-2 w-full">
+    <p class="text-base font-semibold">{product.name}</p>
+    <p class="text-sm">
+      <NumberFlow format={{ style: 'currency', currency: 'USD' }} value={getItemTotalPrice(product.id)} />
+    </p>
+  </div>
+  <div
+    class="flex items-center gap-2 shrink-0 bg-default py-2 border border-default-200 rounded-full w-22 justify-between"
+  >
+    <button class="text-sm font-bold pb-0.5 px-2" onclick={dec}>-</button>
+    <p class="text-sm"><NumberFlow value={quantity} /></p>
+    <button class="text-sm font-bold pb-0.5 px-2" onclick={inc}>+</button>
+  </div>
+</div>
--- apps/frontend/shop/src/entities/cart/ui/sheet/cart-sheet.svelte
+++ apps/frontend/shop/src/entities/cart/ui/sheet/cart-sheet.svelte
@@ -3,11 +3,13 @@
   import { cubicInOut } from 'svelte/easing';
   import { fade, fly } from 'svelte/transition';
   import { cartStore, cartSheetStore, closeCartSheet } from '~/entities/cart';
-  import type { Product } from '~/entities/product';
   import { MOCK_PRODUCTS } from '~/entities/product/config/mock-products';
   import { formatPrice } from '~/shared/lib/utils/price';
+  import CartProduct from './cart-product.svelte';
+  import NumberFlow from '@number-flow/svelte';
 
   const cartItems = $derived(MOCK_PRODUCTS.filter((product) => $cartStore.some((item) => item.id === product.id)));
+
   const totalPrice = $derived(
     $cartStore.reduce((acc, item) => {
       const product = MOCK_PRODUCTS.find((product) => product.id === item.id);
@@ -16,12 +18,6 @@
   );
 
   const close = () => closeCartSheet();
-
-  const getItemTotalPrice = (itemId: string) => {
-    const product = MOCK_PRODUCTS.find((product) => product.id === itemId);
-    const quantity = $cartStore.find((item) => item.id === itemId)?.quantity || 0;
-    return (product?.price || 0) * quantity;
-  };
 
   const handleCheckout = () => {
     close();
@@ -42,19 +38,15 @@
       </div>
       <div class="flex flex-col gap-4 overflow-y-auto h-full">
         {#each cartItems as item (item.id)}
-          <div class="flex items-center gap-4">
-            <img src={item.imageUrl} alt={item.name} class="size-16 rounded-md object-cover" />
-            <div class="flex flex-col gap-2">
-              <p class="text-base font-semibold">{item.name}</p>
-              <p class="text-sm">{formatPrice(getItemTotalPrice(item.id))}</p>
-            </div>
-          </div>
+          <CartProduct product={item} />
         {/each}
       </div>
       <div class="flex flex-col gap-4">
         <div class="flex items-center justify-between">
           <p class="text-base font-bold uppercase">Total</p>
-          <p class="text-base font-bold">{formatPrice(totalPrice)}</p>
+          <p class="text-base font-bold">
+            <NumberFlow format={{ style: 'currency', currency: 'USD' }} value={totalPrice} />
+          </p>
         </div>
         <button
           disabled={cartItems.length === 0}
